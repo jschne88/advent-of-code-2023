24ce13d73fa4b42a3eb13fce02359aa75ec73799
Jared Schneiderman <jared.schneiderman@updater.com>
2023-12-15 13:13:12 -0500

    day 2 part1

---
+++ 2/part1.rb
@@ -0,0 +1,32 @@
+require "pry"
+require "pry-byebug"
+
+file = File.open(ARGV[0])
+lines = file.readlines(chomp: true).map { |line| line }
+
+bag_contents = {
+  red: 12,
+  green: 13,
+  blue: 14
+}
+
+total = lines.reduce(0) do |sum, line|
+  game, game_list = line.split(": ")
+  game_id = game.split(" ")[1]
+
+  is_possible = true
+
+  game_list.split(";").each do |set|
+    set.split(", ").each do |pull|
+      amount, color = pull.split(" ")
+      if bag_contents[color.to_sym] < amount.to_i
+        # binding.pry
+        is_possible = false
+      end
+    end
+  end
+
+  is_possible ? sum + game_id.to_i : sum
+end
+
+puts total
